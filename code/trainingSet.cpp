56247265a019498911b839938ff9f96246ba3d3b
RubickFL <38214905+RubickFL@users.noreply.github.com>
2018-11-25 14:49:51 +0300

    Add files via upload

---
+++ code/trainingSet.cpp
@@ -0,0 +1,81 @@
+/*
+	*This is 'teech' neuronet class
+*/
+
+
+#include "trainingSet.h"
+
+
+TrainingSet::TrainingSet(const std::string filename) {
+	trainingDataFile.open(filename.c_str());
+}
+
+
+
+
+//set topology of neuronet
+void TrainingSet::getTopology(std::vector<unsigned> &topology) {
+	std::string line;
+	std::string label;
+
+
+	std::getline(trainingDataFile, line);
+	std::stringstream ss(line);
+	ss >> label;
+	if (this->isEOF() || label.compare("topology:") != 0) {
+		abort();
+	}
+
+	while (!ss.eof()) {
+		unsigned n;
+		ss >> n;
+		topology.push_back(n);
+	}
+	return;
+}
+
+
+
+
+//get input(s) from data.txt
+unsigned TrainingSet::getNextInputs(std::vector<double> &inputVals) {
+	inputVals.clear();
+
+	std::string line;
+	std::getline(trainingDataFile, line);
+	std::stringstream ss(line);
+
+	std::string label;
+	ss >> label;
+	if (label.compare("in:") == 0) {
+		double oneValue;
+		while (ss >> oneValue) {
+			inputVals.push_back(oneValue);
+		}
+	}
+
+	return inputVals.size();
+}
+
+
+
+
+//get outputs from neuronet
+unsigned TrainingSet::getTargetOutputs(std::vector<double> &targetOutputVals) {
+	targetOutputVals.clear();
+
+	std::string line;
+	std::getline(trainingDataFile, line);
+	std::stringstream ss(line);
+
+	std::string label;
+	ss >> label;
+	if (label.compare("out:") == 0) {
+		double oneValue;
+		while (ss >> oneValue) {
+			targetOutputVals.push_back(oneValue);
+		}
+	}
+
+	return targetOutputVals.size();
+}
